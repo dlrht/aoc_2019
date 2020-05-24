eabee07345f2f19617bbad56f904e0775cb634cb
Omri Cundangan <37829365+dlrht@users.noreply.github.com>
2020-05-24 05:14:24 -0400

    Cleaned up code

--- d9.py
+++ d9.py
@@ -6,6 +6,21 @@ RELATIVE_MODE = 2
 
 READ = 0
 WRITE = 1
+NONE = 99
+
+# Dictionary defining parameter behaviours for each opcode
+op_params = {
+    1 : [READ, READ, WRITE],
+    2 : [READ, READ, WRITE],
+    3 : [WRITE],
+    4 : [READ],
+    5 : [READ, READ],
+    6 : [READ, READ], 
+    7 : [READ, READ, WRITE],
+    8 : [READ, READ, WRITE],
+    9 : [READ],
+    NONE : []
+}
 
 class Intcode():
     def __init__(self, intcode, ip=0, rel_base=0, memory_expand_factor=10):
@@ -17,11 +32,13 @@ class Intcode():
 
     # Returns a len(type_of_param)-tuple with appropriate values based on parameter modes
     # Returns appropriate value if a read command or destination to write to if a write command
-    def get_values(self, modes, type_of_param):
+    def get_values(self, opcode, modes):
+        num_params = len(op_params[opcode])
         values = []
-        for i in range(len(type_of_param)):
+
+        for i in range(num_params):
             val = None
-            if type_of_param[i] == READ:
+            if op_params[opcode][i] == READ:
                 if modes[i] == POSITION_MODE:
                     val = self.intcode[self.intcode[self.ip+i+1]]
                 elif modes[i] == IMMEDIATE_MODE:
@@ -29,7 +46,7 @@ class Intcode():
                 elif modes[i] == RELATIVE_MODE:
                     val = self.intcode[self.rel_base + self.intcode[self.ip+i+1]]
 
-            elif type_of_param[i] == WRITE:
+            elif op_params[opcode][i] == WRITE:
                 if modes[i] == POSITION_MODE:
                     val = self.intcode[self.ip+i+1]
                 elif modes[i] == RELATIVE_MODE:
@@ -40,7 +57,7 @@ class Intcode():
 
             values.append(val)
 
-        return tuple(values) if len(type_of_param) > 1 else values[0]
+        return tuple(values)
 
 
     def Execute(self, input_set=[], new_ip=None, new_rel_base=None, halt_on_empty_input_set=False, verbose=0):
@@ -62,16 +79,18 @@ class Intcode():
                 print_intcode[self.ip] = '***' + str(print_intcode[self.ip]) + '***'
                 print(print_intcode, "-", "ip:"+str(self.ip), "opcode:"+str(opcode), "modes:"+str(parameter_modes))
 
+            params = self.get_values(opcode, parameter_modes)
+            x = params[0] if len(params) >= 1 else None
+            y = params[1] if len(params) >= 2 else None
+            z = params[2] if len(params) >= 3 else None
+
             if opcode == 1:     # Addition
-                x,y,z = self.get_values(parameter_modes, [READ, READ, WRITE])
                 self.intcode[z] = x+y
                 self.ip += 4
             elif opcode == 2:   # Multiplication
-                x,y,z = self.get_values(parameter_modes, [READ, READ, WRITE])
                 self.intcode[z] = x*y
                 self.ip += 4
             elif opcode == 3:   # Single int input
-                x = self.get_values(parameter_modes, [WRITE])                
                 if len(input_set) > 0:  # Inputs the values in input_set
                     self.intcode[x] = input_set.pop(0)
                     self.ip += 2
@@ -84,25 +103,19 @@ class Intcode():
                     self.intcode[x] = int(user_input)
                     self.ip += 2
             elif opcode == 4:   # Outputs value of only parameter and stores to output list
-                src = self.get_values(parameter_modes, [READ])
-                outputs.append(src)
+                outputs.append(x)
                 self.ip += 2           
             elif opcode == 5:   # jump-if-true
-                x,y = self.get_values(parameter_modes, [READ, READ])
                 self.ip = y if x != 0 else self.ip + 3
             elif opcode == 6:   # jump-if-false
-                x,y = self.get_values(parameter_modes, [READ, READ])
                 self.ip = y if x == 0 else self.ip + 3
             elif opcode == 7:   # less than
-                x,y,z = self.get_values(parameter_modes, [READ, READ, WRITE])
                 self.intcode[z] = 1 if x < y else 0
                 self.ip += 4
             elif opcode == 8:   # equals
-                x,y,z = self.get_values(parameter_modes, [READ, READ, WRITE])
                 self.intcode[z] = 1 if x == y else 0
                 self.ip += 4
             elif opcode == 9:   # set relative base
-                x = self.get_values(parameter_modes, [READ])
                 self.rel_base += x
                 self.ip += 2
             elif opcode == 99: # Halt opcode
